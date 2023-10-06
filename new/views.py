f1ec2bc53e7b601019cdaabb180b15dfb9955703
naflaa <naflathahir100@gmail.com>
2023-10-06 13:18:19 +0530

    first commit

---
+++ new/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from . models import Address
+from django.http import JsonResponse
+from .serilaizer import AddressSerilaizer
+from rest_framework.decorators import api_view
+
+@api_view(['GET','POST'])
+def index(request):
+    if request.method=="GET":
+        address=Address.objects.all()
+        serilaizer=AddressSerilaizer(address, many=True)
+        return JsonResponse(serilaizer.data, safe=False)
+    if request.method=='POST':
+        serilaizer=AddressSerilaizer(data=request.data)
+        if serilaizer.is_valid():
+            serilaizer.save()
+            return JsonResponse(serilaizer.data, safe=False)
+    return JsonResponse()
+@api_view(['GET','POST','DELETE'])
+def hello(request,id):
+    try:
+        address=Address.objects.get(pk=id)
+    except Address.DoesNotExist:
+        return JsonResponse({'Error':'Address not found'}, status=404)
+    if request.method=='GET':
+        serilaizer=AddressSerilaizer(address)
+        return JsonResponse(serilaizer.data)
+    elif request.method=='DELETE':
+        address.delete()
+        return JsonResponse({'Deleted':'Address Deleted'}, status=204)
+    return render(request,"hello.html")
